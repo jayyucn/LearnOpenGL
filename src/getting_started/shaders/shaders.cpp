6f8dd5f6670be6e138edaa6e5e783b46c8e3228e
jayyucn <nalanrola@gmail.com>
2023-07-10 15:07:48 +0800

    [fix] fix include error

--- src/getting_started/shaders/shaders.cpp
+++ src/getting_started/shaders/shaders.cpp
@@ -2,7 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include "shader_s.h"
+#include <learnopengl/shader_s.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
